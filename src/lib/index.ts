3604350f81591b7066a71ada4895385b02433a4e
Syed Ali Murtaza <ali@kazmi.me>
2022-06-19 18:26:31 +0500

    refactor ng- to bo-

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@
  * Public API Surface of bo-configuration
  */
 
-export * from './bo-configuration.module';
+export * from './configuration.module';
 export * from './models';
 export * from './configuration.sandbox';
 
